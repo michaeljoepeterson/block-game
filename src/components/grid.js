175e675ed49aea304729b2766a8b2b1c5b907a29
Michael Peterson <michaeljoepeterson@gmail.com>
2019-01-13 14:35:49 -0700

    set up functions to allow drag over grid funcitonality

--- src/components/grid.js
+++ src/components/grid.js
@@ -3,6 +3,11 @@ import {connect} from 'react-redux';
 import "./grid.css";
 
 export class Grid extends React.Component{
+	constructor(props){
+		super(props);
+		this.gridButtonClickState = false;
+	}
+
 	componentDidMount() {
 		console.log("test");
     }
@@ -10,7 +15,6 @@ export class Grid extends React.Component{
     setRowData(numCol){
     	let returnArray = []
     	for(let i = 0; i < numCol;i++){
-    		//console.log("run for",numCol);
     		returnArray.push({id:i});
     	}
     	return returnArray;
@@ -19,18 +23,33 @@ export class Grid extends React.Component{
     setColumnData(numRow){
     	let returnArray = []
     	for(let i = 0; i < numRow;i++){
-    		//console.log("run for",numRow);
 
     		returnArray.push({id:i} );
     	}
     	return returnArray;
     }
-    //need to rethink how the row/column data saved due to issues with double digit grids
-    //ie when grid double digits can have multiple spaces where grid button id is the same
+    //will need to convert the grid array to data that can be used to color the squares
+    //idea is that while dragging see if the mouse went over the grid block then save that info in array data
+    //then go through that array and color the corresponding squares
+    gridButtonMouseOver(event){
+    	event.preventDefault();
+    	console.log("the mouse is over: ",event.target.id);
+    }
+
+    gridbuttonMouseUp(event){
+    	event.preventDefault();
+    	this.gridButtonClickState = false;
+    	console.log("mouse up, variable state: ",this.gridButtonClickState);
+    }
+
+    gridButtonMouseDown(event){
+    	event.preventDefault();
+    	this.gridButtonClickState = true;
+    	console.log("mouse held down, variable state: ",this.gridButtonClickState);
+    }
+
     gridButtonClicked(event){
     	event.preventDefault();
-    	//console.log(event.target.dataset.row)
-    	//console.log(event.target.dataset.column)
     	let buttonId = event.target.id;
     	console.log("button clicked, id: ",event.target.id);
     	let targetButton = document.getElementById(buttonId);
@@ -64,7 +83,7 @@ export class Grid extends React.Component{
 		try{
 			rowData = this.setRowData(this.props.gridData.rows);
 			columnData = this.setColumnData(this.props.gridData.columns)
-			console.log("column data", columnData);
+			console.log("grid data", this.props.gridData.gridArray);
 			if(this.props.gridData.columns > 0 && this.props.gridData.rows > 0){
 				console.log("construct table");
 				tableData = rowData.map(row=>{
@@ -74,7 +93,9 @@ export class Grid extends React.Component{
 								return(
 									<td 
 									key={row.id + column.id}><button id={"row" + row.id.toString() + " " + "column" + column.id.toString()} className="gridButton" data-row={row.id} data-column={column.id}
-									onClick={(e)=>this.gridButtonClicked(e)}
+									onMouseDown={(e)=>{this.gridButtonClicked(e); this.gridButtonMouseDown(e);}}
+									onMouseUp={(e) => this.gridbuttonMouseUp(e)}
+									onMouseOver = {(e) => this.gridButtonMouseOver(e)}
 									></button>
 									</td>
 								);
--- src/reducers/gridReducer.js
+++ src/reducers/gridReducer.js
@@ -9,8 +9,22 @@ const initialState = {
 	columns:null,
 	loading:null,
 	message:null,
-	name:null
+	name:null,
+	gridArray:null
 };
+
+function createGridArray(rows,columns){
+	let gridArr = [];
+	let rowArr = [];
+	for(let i = 0;i < rows; i++){
+		rowArr = [];
+		for(let k = 0; k < columns;k++){
+			rowArr.push(0);
+		}
+		gridArr.push(rowArr);
+	}
+	return gridArr;
+}
 
 export default function reducer(state = initialState,action){
 	//console.log(state);
@@ -23,13 +37,16 @@ export default function reducer(state = initialState,action){
 	}
 	else if(action.type === CREATE_GRID_SUCCESS){
 		console.log("reducer success data", action.gridData);
+		const gridArray = createGridArray(parseInt(action.gridData.rows),parseInt(action.gridData.columns));
+		console.log("reducer gridArray: ",gridArray);
 		return Object.assign({},state,{
 			loading:null,
 			message:"success",
 			error:null,
 			name:action.gridData.gameName,
 			rows:action.gridData.rows,
-			columns:action.gridData.columns
+			columns:action.gridData.columns,
+			gridArray
 		});
 	}
 	else if(action.type === CREATE_GRID_ERROR){
